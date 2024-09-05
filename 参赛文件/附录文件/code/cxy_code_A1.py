73392a553fbe58d7a8a836001758e3b435ad724f
Xuanyu Chen <150888349+lava-chen@users.noreply.github.com>
2024-09-05 22:43:15 +0800

    python虚拟环境配置

---
+++ 参赛文件/附录文件/code/cxy_code_A1.py
@@ -0,0 +1,19 @@
+import math
+import sympy
+class dragonhead:
+    speed = 1
+    def __init__(self,x,y):
+        pass
+        self.x = x
+        self.y = y
+    def x_position(theta):
+        x = (0.55/(2*math.pi))*theta*math.cos(theta)
+    def y_position(theta):
+        y = (0.55/(2*math.pi))*theta*math.sin(theta)
+class dragonbody:
+    def __init__(self,n):
+        self.n=n
+def theta(t):
+
+def position_dragonhead():
+
